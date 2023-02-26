1854fe5dd606b7e98339facd6e0ec0802648dfe7
stan-miniu <121545331+stan-miniu@users.noreply.github.com>
2023-02-26 16:57:22 +0100

    Wersja ze zmianą prędkości i hamowaniem

--- main.ts
+++ main.ts
@@ -5,7 +5,20 @@ bluetooth.onBluetoothDisconnected(function () {
     basic.showIcon(IconNames.No)
 })
 control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EVT_ANY, function () {
-    if (control.eventValue() == KLAW_PRZOD_NACISNIETY) {
+    if (control.eventValue() == KLAW_A_NACISNIETY || control.eventValue() == KLAW_A_PUSZCZONY) {
+        maqueen.motorStop(maqueen.Motors.All)
+        kierunek = STOP
+        predkosc = STOP
+        predkosc_lewy = STOP
+        predkosc_prawy = STOP
+        basic.showLeds(`
+            . # # # .
+            # . . . #
+            # # # # #
+            # . . . #
+            . # # # .
+            `)
+    } else if (control.eventValue() == KLAW_PRZOD_NACISNIETY) {
         if (kierunek == KIER_PRZOD) {
             if (predkosc < PREDKOSC_MAX) {
                 predkosc += PREDKOSC_ZMIANA
@@ -14,8 +27,8 @@ control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EV
             if (predkosc > PREDKOSC_MIN) {
                 predkosc += PREDKOSC_ZMIANA * -1
             } else {
-                kierunek = KIER_STOP
-                predkosc = PREDKOSC_STOP
+                kierunek = STOP
+                predkosc = 0
             }
         } else {
             kierunek = KIER_PRZOD
@@ -32,8 +45,8 @@ control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EV
             if (predkosc > PREDKOSC_MIN) {
                 predkosc += PREDKOSC_ZMIANA * -1
             } else {
-                kierunek = KIER_STOP
-                predkosc = PREDKOSC_STOP
+                kierunek = STOP
+                predkosc = STOP
             }
         } else {
             kierunek = KIER_TYL
@@ -43,9 +56,9 @@ control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EV
         predkosc_prawy = predkosc
     } else if (control.eventValue() == KLAW_LEWO_NACISNIETY) {
         if (kierunek == KIER_PRZOD) {
-            predkosc_lewy = PREDKOSC_STOP
+            predkosc_lewy = STOP
         } else if (kierunek == KIER_TYL) {
-            predkosc_prawy = PREDKOSC_STOP
+            predkosc_prawy = STOP
         } else {
             predkosc_prawy = PREDKOSC_MIN
         }
@@ -56,14 +69,11 @@ control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EV
             . . . # .
             . . # . .
             `)
-    } else if (control.eventValue() == KLAW_LEWO_PUSZCZONY) {
-        predkosc_lewy = predkosc
-        predkosc_prawy = predkosc
     } else if (control.eventValue() == KLAWISZ_PRAWO_NACISNIETY) {
         if (kierunek == KIER_PRZOD) {
-            predkosc_prawy = PREDKOSC_STOP
+            predkosc_prawy = STOP
         } else if (kierunek == KIER_TYL) {
-            predkosc_lewy = PREDKOSC_STOP
+            predkosc_lewy = STOP
         } else {
             predkosc_lewy = PREDKOSC_MIN
         }
@@ -74,7 +84,7 @@ control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EV
             . # . . .
             . . # . .
             `)
-    } else if (control.eventValue() == KAWISZ_PRAWO_PUSZCZONY) {
+    } else if (control.eventValue() == KLAW_LEWO_PUSZCZONY || control.eventValue() == KAWISZ_PRAWO_PUSZCZONY) {
         predkosc_lewy = predkosc
         predkosc_prawy = predkosc
     }
@@ -82,41 +92,29 @@ control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EV
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, predkosc_lewy)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, predkosc_prawy)
         if (predkosc_lewy == predkosc_prawy) {
-            basic.showLeds(`
-                . . # . .
-                . . # . .
-                # . # . #
-                . # # # .
-                . . # . .
-                `)
+            basic.showNumber((predkosc + 1) / PREDKOSC_ZMIANA)
         }
     } else if (kierunek == KIER_TYL) {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, predkosc_lewy)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, predkosc_prawy)
         if (predkosc_lewy == predkosc_prawy) {
-            basic.showLeds(`
-                . . # . .
-                . # # # .
-                # . # . #
-                . . # . .
-                . . # . .
-                `)
+            basic.showNumber((predkosc + 1) / PREDKOSC_ZMIANA)
         }
     } else {
-        if (predkosc_lewy > PREDKOSC_STOP) {
+        if (predkosc_lewy > STOP) {
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, predkosc_lewy)
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, predkosc_lewy)
-        } else if (predkosc_prawy > PREDKOSC_STOP) {
+        } else if (predkosc_prawy > STOP) {
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, predkosc_prawy)
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, predkosc_prawy)
         } else {
             maqueen.motorStop(maqueen.Motors.All)
             basic.showLeds(`
+                . # # # .
                 # . . . #
-                . # . # .
-                . . # . .
-                . # . # .
                 # . . . #
+                # . . . #
+                . # # # .
                 `)
         }
     }
@@ -128,16 +126,19 @@ let predkosc = 0
 let PREDKOSC_ZMIANA = 0
 let PREDKOSC_MAX = 0
 let PREDKOSC_MIN = 0
-let PREDKOSC_STOP = 0
 let KIER_TYL = 0
 let KIER_PRZOD = 0
-let KIER_STOP = 0
+let STOP = 0
 let KAWISZ_PRAWO_PUSZCZONY = 0
 let KLAWISZ_PRAWO_NACISNIETY = 0
 let KLAW_LEWO_PUSZCZONY = 0
 let KLAW_LEWO_NACISNIETY = 0
 let KLAW_TYL_NACISNIETY = 0
 let KLAW_PRZOD_NACISNIETY = 0
+let KLAW_A_PUSZCZONY = 0
+let KLAW_A_NACISNIETY = 0
+KLAW_A_NACISNIETY = 1
+KLAW_A_PUSZCZONY = 2
 KLAW_PRZOD_NACISNIETY = 9
 let KLAW_PRZOD_PUSZCZONY = 10
 KLAW_TYL_NACISNIETY = 11
@@ -147,10 +148,9 @@ KLAW_LEWO_PUSZCZONY = 14
 KLAWISZ_PRAWO_NACISNIETY = 15
 KAWISZ_PRAWO_PUSZCZONY = 16
 bluetooth.startUartService()
-KIER_STOP = 0
+STOP = 0
 KIER_PRZOD = 1
 KIER_TYL = -1
-PREDKOSC_STOP = 0
 PREDKOSC_MIN = 63
 PREDKOSC_MAX = 255
 PREDKOSC_ZMIANA = 64
